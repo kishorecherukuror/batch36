8b675f6c1c19330773a7a6c1f749a11d8d040611
Nikunj Ukani <ukaninikunj@gmail.com>
2016-11-28 12:55:16 -0800

    created post scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124165533) do
+ActiveRecord::Schema.define(version: 20161128205333) do
 
   create_table "employees", force: :cascade do |t|
     t.string   "name"
     t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
